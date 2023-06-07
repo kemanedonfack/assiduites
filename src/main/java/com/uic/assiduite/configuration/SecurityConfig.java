c749f0429a471adb12f512c8a3034b8da3acce9f
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-08 00:53:51 +0100

    allow swagger path on spring security

--- src/main/java/com/uic/assiduite/configuration/SecurityConfig.java
+++ src/main/java/com/uic/assiduite/configuration/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-                "/configuration/security", "/swagger-ui.html", "/swagger-ui.html#", "/webjars/**");
+                "/configuration/security", "/swagger-ui/**", "/webjars/**");
     }
 
     @Bean
