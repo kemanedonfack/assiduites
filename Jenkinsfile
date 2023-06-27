002051fc0ffbfd76ece58c72122b6929f935c962
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-27 14:15:06 +0100

    add zap script

--- Jenkinsfile
+++ Jenkinsfile
@@ -41,9 +41,7 @@ pipeline {
 
         stage('owasp zap scan') {
             steps {
-                sh 'docker pull owasp/zap2docker-weekly'
-                sh 'chmod 777 $(pwd)'
-                sh 'docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t  http://13.49.76.44:8090/v3/api-docs -f openapi -r zap_report-${gitCommit}.html'
+                sh 'bash zap.sh'
                 sh 'aws s3 cp zap_report-${gitCommit}.html ${s3buckect}/'
             }
         }
---
+++ zap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# first run this
+chmod 777 $(pwd)
+
+# comment above cmd and uncomment below lines to run with CUSTOM RULES
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t  http://13.49.76.44:8090/v3/api-docs -f openapi -r zap_report-${gitCommit}.html
+exit_code=$?
+
+
+# HTML Report
+
+echo "Exit Code : $exit_code"
+
+ if [[ ${exit_code} -ne 0 ]];  then
+    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+    exit 1;
+   else
+    echo "OWASP ZAP did not report any Risk"
+ fi;
